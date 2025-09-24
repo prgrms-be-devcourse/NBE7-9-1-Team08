f5b4e8ca5c61d68df47ba1a1056c1c627c9edc58
caketower <dlaqudtn1107@naver.com>
2025-09-24 09:44:14 +0900

    상품 등록 조회 기능추가

--- src/main/java/org/example/cafe/domain/product/controller/ProductController.java
+++ src/main/java/org/example/cafe/domain/product/controller/ProductController.java
@@ -5,6 +5,8 @@ import org.example.cafe.domain.product.entity.Product;
 import org.example.cafe.domain.product.service.ProductService;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -24,7 +26,16 @@ public class ProductController {
     }
 
     //상품 등록
-
+    @PostMapping("/api/products")
+    @Transactional
+    public Product createProduct(
+            @RequestBody String name,
+            @RequestBody int price,
+            @RequestBody int quantity
+    ){
+        Product product = productService.create("name", price, quantity);
+        return product;
+    }
 
     //상품 수정
 
